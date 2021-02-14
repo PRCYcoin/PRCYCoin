f887433a05cd5dd7facda0f2566d9b3eca590eb3
lopeed_prcy <35898975+lopeed@users.noreply.github.com>
2021-02-14 12:27:15 +0100

    Asigned new value to N_BITS_SF

--- src/poa.cpp
+++ src/poa.cpp
@@ -17,7 +17,7 @@
 #include <math.h>
 
 unsigned int N_BITS = 0x1e050000;
-unsigned int N_BITS_SF = 0x1e050000;
+unsigned int N_BITS_SF = 0x1e127ff8;
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader* pblock)
 {
