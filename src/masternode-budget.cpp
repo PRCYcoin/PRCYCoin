efd3a45b4abe2807ca7fb6b9724ca98206930d43
lopeed_prcy <35898975+lopeed@users.noreply.github.com>
2021-03-09 01:59:31 +0100

    [Trivial] Fix error messages: Setkey -> GetKeysFromSecret

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -2006,7 +2006,7 @@ void CFinalizedBudget::SubmitVote()
     }
 
     if (!CMessageSigner::GetKeysFromSecret(strMasterNodePrivKey, keyMasternode, pubKeyMasternode)) {
-        LogPrint("masternode","CFinalizedBudget::SubmitVote - Error upon calling SetKey\n");
+        LogPrint("mnbudget","CFinalizedBudget::SubmitVote - Error upon calling GetKeysFromSecret\n");
         return;
     }
 
